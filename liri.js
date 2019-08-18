6b58342e4e51c0fa16cb09c33a7086359e89a642
Manseerat Kaur <seeratsidhu01@gmail.com>
2019-08-18 18:39:25 -0400

    updated the bandsintown function

--- liri.js
+++ liri.js
@@ -24,15 +24,17 @@ switch (command) {
         OMDBSearch(parameter);
         break;
     case "do-what-it-says":
-
-
+        doFile();
 
 }
 
 function bandsinTown(parameter) {
     queryURL = "https://rest.bandsintown.com/artists/" + parameter + "/events?app_id=codingbootcamp";
     Axios.get(queryURL).then(response => {
-        console.log(response);
+        console.log(response.data);
+        console.log("Name of Venue: " + response.data[9].venue.name);
+        console.log("Venue location: " + response.data[9].venue.city + ", " + response.data[1].venue.country);
+        console.log("Date of the Event: " + moment().format("MM/DD/YYYY", response.data[9].datatime));
     });
 };
 
@@ -41,6 +43,9 @@ function spotifySearch(parameter) {
         .search({ type: 'track', query: parameter })
         .then(response => {
             console.log(response.tracks.items[0]);
+            console.log("Artist(s): " + response.tracks.items[0].album.artists.name);
+            console.log("Song Name: " + response.tracks.items[0].album.external_urls.name);
+            // console.log("Link to Spotify: " + response.album.)
         })
         .catch(err => {
             console.log(err);
@@ -57,5 +62,32 @@ function OMDBSearch(parameter) {
             data.imdbRating + "\nRotten Tomatoes rating: " + data.Ratings[1].Value + "\nCountry of Production: " +
             data.Country + "\nLanguage: " + data.Language + "\nPlot: " + data.Plot + "\nActors: " + data.Actors);
 
+    });
+}
+
+function doFile() {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            return console.error(err);
+        }
+
+        var dataArr = data.split(",");
+        console.log(dataArr);
+        var indexSpotify = dataArr.indexOf("spotify-this-song");
+        var indexMovie = dataArr.indexOf("movie-this");
+        var indexBands = dataArr.indexOf("concert-this");
+        if (dataArr[indexSpotify]) {
+
+            spotifySearch(dataArr[indexSpotify + 1]);
+        }
+
+        else if (dataArr[indexMovie]){
+    
+        OMDBSearch(dataArr[indexMovie + 1]);
+        }
+        else if (dataArr[indexBands]){
+        bandsinTown(dataArr[indexBands + 1]);
+        }
+    
     });
 }
